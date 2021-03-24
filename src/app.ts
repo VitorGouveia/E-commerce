b172104783af44303239c95aebe7a9cd75158df9
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-03-24 04:41:39 -0300

    creates routes

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express"
 import { config as dotenv } from "dotenv"
+import { router } from "./routes"
 
 const app = express()
 
 app.use(express.json())
+app.use(router)
 
 dotenv({ path: `${__dirname}/config/.env` })
 
