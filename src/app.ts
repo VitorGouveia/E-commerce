bb58172f294491492a73df13554223e3cf0301fb
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-03-24 04:43:03 -0300

    adds and configures cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express"
+import cors from "cors"
 import { config as dotenv } from "dotenv"
 import { router } from "./routes"
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 dotenv({ path: `${__dirname}/config/.env` })
