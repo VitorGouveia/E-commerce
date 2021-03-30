2a2b1513b54c52f0e5d4dfbd555edf31dd6dff87
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-03-30 17:38:48 -0300

    Adds user register test

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -1,7 +1,25 @@
 import request from "supertest"
+import { app } from "../app"
+import { User } from "../entities/User"
 
-describe("register", () => {
-  it("should create new user", () => {
-    
+const CreateUserRequest = {
+  name: "vitor",
+  email: "vitor@test.com",
+  password: "123"
+}
+
+describe("User Register", () => {
+  it("should create new user entity", async () => {
+    let { name, email, password } = CreateUserRequest
+
+    const user = new User({ name, email, password })
+
+    expect(user.uuid).toHaveLength(40)
+    expect(user.name).toBe(CreateUserRequest.name)
+    expect(user.email).toBe(CreateUserRequest.email)
+
+    // const response = await request(app).post("/user").send(user)
   })
+
+  // it("should")
 })
