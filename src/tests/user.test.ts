07f14eeebd6608c9fa1b9b005806cf784ab7651c
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-10 23:47:53 -0300

    Adds User Register tests

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -15,23 +15,27 @@ const user = new User({ name, email, password })
 
 describe("User Register", () => {
   it("should create new user entity", async () => {
-
-
     expect(user.uuid).toHaveLength(40)
     expect(user.name).toBe(CreateUserRequest.name)
     expect(user.email).toBe(CreateUserRequest.email)
   })
   
-  it("should pass user register route", async () => {
+  it("should receive a 200 or 400 status code when creating new user", async () => {
     const response = await request(app).post("/user").send(user)
 
     expect(response.status == 200 || response.status == 400).toBe(true)
   })
 
-  it("should store user in the database", async () => {
+  it("should receive JWT_ACESS_TOKEN", async () => {
     const response = await request(app).post("/user").send(user)
 
-    FindByEmail(user.email, (rows) => {
+    expect(response.body["auth"] == false || response.body["auth"] == true).toBe(true)
+  })
+
+  it("should store user in the database", async () => {
+    await request(app).post("/user").send(user)
+
+    FindByEmail(user.email, (rows: any) => {
       rows.forEach(user2 => {
         expect(user.name).toBe(user2.name)
         expect(user.email).toBe(user2.email)
