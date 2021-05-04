fa3a2bdbd0657f8dbe8f977a5b31638ef7a43f40
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-05-04 07:13:17 -0300

    add: responsive bugfixes

--- docs/scripts/createComponents.js
+++ docs/scripts/createComponents.js
@@ -9,14 +9,27 @@ const createComponent = async (component, data) => {
   newComponent.innerHTML = data
 
   document.querySelector(".homeContainer").appendChild(newComponent)
-
-  const links = document.querySelectorAll("a")
   
+  const links = document.querySelectorAll("a")
   links.forEach(link => {
     link.addEventListener("click", event => {
       document.querySelector(".active-link").classList.remove("active-link")
       event.target.classList.add("active-link")
     })
+  })
+
+  const buttons = document.querySelectorAll("button")
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      document.querySelector(".homeContainer aside").classList.toggle("show")
+      document.querySelector(".overlay").style.display = "block"
+      
+    })
+  })
+
+  document.querySelector(".overlay").addEventListener("click", () => {
+    document.querySelector(".homeContainer aside").classList.remove("show")
+    document.querySelector(".overlay").style.display = "none"
   })
 }
 
