ddf9c64ad1a0f2bb5edf4b8be667921bfb85da0d
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-06-20 21:11:28 -0300

    put all controllers into one file

---
+++ src/api/v1.0/controllers/index.ts
@@ -0,0 +1,6 @@
+import { UserController } from "./UserController"
+import { ItemController } from "./ItemController"
+import { SessionController } from "./SessionController"
+import { DashboardController } from "./DashboardController"
+
+export { UserController, ItemController, SessionController, DashboardController }
