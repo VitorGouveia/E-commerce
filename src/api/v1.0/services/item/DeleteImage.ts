b3ded00fb95328482b6752a75d0da96f79be3137
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-01 03:08:08 -0300

    removes unused import and changes methods names

--- src/api/v1.0/services/item/DeleteImage.ts
+++ src/api/v1.0/services/item/DeleteImage.ts
@@ -3,16 +3,13 @@ import { Request } from "express"
 import { IImageRepository } from "@v1/repositories"
 import { SqliteImageRepository } from "@v1/repositories/implementations"
 
-import { Image } from "@v1/entities"
-
 export class DeleteImageService {
   constructor(
     private imageRepository: IImageRepository
   ) {}
 
-  async create({ id }: any, item_id: number) {
+  async delete({ id }: any, item_id: number) {
     try {
-      console.log(id)
       await this.imageRepository.delete(Number(id), item_id)
     } catch (error) {
       throw new Error(error.message)
@@ -23,9 +20,9 @@ export class DeleteImageService {
 export default async (request: Request) => {
   try {
     const sqliteImageRepository = new SqliteImageRepository()
-    const CreateImage = new DeleteImageService(sqliteImageRepository)
+    const DeleteImage = new DeleteImageService(sqliteImageRepository)
 
-    await CreateImage.create(request.body, Number(request.params.id))
+    await DeleteImage.delete(request.body, Number(request.params.id))
 
     return ({
       status: 200,
