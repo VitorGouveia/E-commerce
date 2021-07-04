1a8e8af45e35d9909b645950022305865d6c3d31
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-04 00:07:13 -0300

    fix: updates babel's module-resolver paths to match tsconfig paths

--- babel.config.js
+++ babel.config.js
@@ -13,9 +13,15 @@ module.exports = {
   plugins: [
     ["module-resolver", {
       alias: {
-        "@controllers": "./src/controllers",
-        "@tests": "./src/tests",
-        "@utils": "./src/utils"
+        "@src": "./src",
+        "@v1": "./src/api/v1.0",
+        "@v1/auth": "./src/api/v1.0/auth",
+        "@v1/routes": "./src/api/v1.0/routes",
+        "@v1/utils": "./src/api/v1.0/utils",
+        "@v1/entities": "./src/api/v1.0/entities",
+        "@v1/repositories": "./src/api/v1.0/repositories",
+        "@v1/controllers": "./src/api/v1.0/controllers",
+        "@v1/services": "./src/api/v1.0/services",
       }
     }]
   ],
