ccda95dbd87ecc2c353638548ef6b260324bb9c8
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-06 03:24:08 -0300

    :package: add: ip ban test

---
+++ src/tests/IpBan.test.ts
@@ -0,0 +1,14 @@
+type ApiResponse = {
+	status: number;
+	body: {
+		message: string;
+	};
+};
+
+describe('IP ban', () => {
+	it('should be IP banned in user routes', async () => {
+		/**
+		 * Need to find a way to change supertest request IP so I can see if API really blocks it
+		 */
+	});
+});
