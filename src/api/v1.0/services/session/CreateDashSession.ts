a4ca2e0c5c646e6d983c7e00f094afabf8f16264
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-07 00:26:48 -0300

    add: linting and refactor of create dash session

--- src/api/v1.0/services/session/CreateDashSession.ts
+++ src/api/v1.0/services/session/CreateDashSession.ts
@@ -1,108 +1,104 @@
-import { Request } from "express"
-import { sign, verify } from "jsonwebtoken"
-import { compare } from "bcrypt"
+import { Request } from 'express';
+import { sign, verify } from 'jsonwebtoken';
+import { compare } from 'bcrypt';
 
-import { IUsersRepository } from "@v1/repositories"
-import { SqliteUsersRepository } from "@v1/repositories/implementations"
+import { IUsersRepository } from '@v1/repositories';
+import { SqliteUsersRepository } from '@v1/repositories/implementations';
 
-import { User } from "@v1/entities"
+import { User } from '@v1/entities';
 
 type LoginRequestType = {
-  email: string
-  password: string
-}
+	email: string;
+	password: string;
+};
 
 class CreateDashSessionService {
-  constructor(
-    private usersRepository: IUsersRepository
-  ) {}
+	constructor(private usersRepository: IUsersRepository) {}
 
-  async login(loginRequest: LoginRequestType, authHeader: string | undefined) {
-    try {
-      const dash_access_token = String(process.env.DASH_ACCESS_TOKEN)
-      const dash_refresh_token = String(process.env.DASH_REFRESH_TOKEN)
+	async login(loginRequest: LoginRequestType, authHeader: string | undefined) {
+		try {
+			const dash_access_token = String(process.env.DASH_ACCESS_TOKEN);
+			const dash_refresh_token = String(process.env.DASH_REFRESH_TOKEN);
 
-      const token = authHeader?.split(" ")[1]
-
-      if(authHeader == undefined) throw new Error("No token was found")
-      if(token == undefined) throw new Error(`
+			const token = authHeader?.split(' ')[1];
+			if (authHeader == undefined) throw new Error('No token was found');
+			if (token == undefined)
+				throw new Error(`
         Your token must include Bearer:\n
           example: 'Bearer <your_token>'
-      `)
+      `);
 
-      const payload = verify(token, dash_access_token)
-      const jwt_user = {
-        id: payload["id"],
-        name: payload["name"],
-        email: payload["email"]
-      }
+			const payload = verify(token, dash_access_token);
+			const jwt_user = {
+				id: payload['id'],
+				name: payload['name'],
+				email: payload['email'],
+			};
 
-      const refresh_token = sign(
-        jwt_user,
-        dash_refresh_token,
-        { expiresIn: "168h" } // 7 days
-      )
+			const refresh_token = sign(
+				jwt_user,
+				dash_refresh_token,
+				{ expiresIn: '168h' } // 7 days
+			);
 
-      return ({
-        jwt_login: true,
-        refresh_token
-      })
-    } catch (error) {
-      const { email, password } = loginRequest
+			return {
+				jwt_login: true,
+				refresh_token,
+			};
+		} catch (error) {
+			const { email, password } = loginRequest;
 
-      const user = await this.usersRepository.findByEmail(email)
+			const user = await this.usersRepository.findByEmail(email);
 
-      if(user.length == 0) throw new Error("Wrong e-mail!")
+			if (user.length == 0) throw new Error('Wrong e-mail!');
 
-      const comparePassword = await compare(password, user[0].password)
+			const comparePassword = await compare(password, user[0].password);
 
-      if(comparePassword != true) throw new Error("Wrong password!")
+			if (comparePassword != true) throw new Error('Wrong password!');
 
-      const dash_refresh_token = String(process.env.DASH_REFRESH_TOKEN)
+			const dash_refresh_token = String(process.env.DASH_REFRESH_TOKEN);
 
-      const [{ id, name }] = user
-      const jwt_user = {
-        id,
-        name,
-        email: user[0].email
-      }
+			const [{ id, name }] = user;
+			const jwt_user = {
+				id,
+				name,
+				email: user[0].email,
+			};
 
-      const refresh_token = sign(
-        jwt_user,
-        dash_refresh_token,
-        { expiresIn: "168h" }
-      )
+			const refresh_token = sign(jwt_user, dash_refresh_token, {
+				expiresIn: '168h',
+			});
 
-      return ({
-        social_login: true,
-        refresh_token
-      })
-    }
-  }
+			return {
+				social_login: true,
+				refresh_token,
+			};
+		}
+	}
 }
 
 export default async (request: Request) => {
-  try {
-    const UsersRepository = new SqliteUsersRepository()
-    const CreateDashSession = new CreateDashSessionService(UsersRepository)
+	try {
+		const UsersRepository = new SqliteUsersRepository();
+		const CreateDashSession = new CreateDashSessionService(UsersRepository);
 
-    const {
-      refresh_token,
-      jwt_login,
-      social_login
-    } = await CreateDashSession.login(request.body, request.headers["authorization"])
+		const { refresh_token, jwt_login, social_login } =
+			await CreateDashSession.login(
+				request.body,
+				request.headers['authorization']
+			);
 
-    return ({
-      jwt_login,
-      social_login,
-      status: 200,
-      refresh_token
-    })
-  } catch (error) {
-    return ({
-      error: true,
-      status: 400,
-      message: "Failed to login as an Admin."
-    })
-  }
-}
+		return {
+			jwt_login,
+			social_login,
+			status: 200,
+			refresh_token,
+		};
+	} catch (error) {
+		return {
+			error: true,
+			status: 400,
+			message: 'Failed to login as an Admin.',
+		};
+	}
+};
