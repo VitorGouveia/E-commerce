0fc0bd1f16a92438391485597ce716ebb24f3b7d
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-10 20:23:07 -0300

    chore: increase rate limiter requests limt

--- src/api/v1.0/routes/middlewares/rateLimit.ts
+++ src/api/v1.0/routes/middlewares/rateLimit.ts
@@ -3,14 +3,13 @@ import rateLimit from 'express-rate-limit';
 export const CreateUserRateLimiter = () =>
 	rateLimit({
 		windowMs: 60 * 60 * 1000, // 1 hour window
-		max: 5, // max requests
-		message:
-			'Too many accounts created from this IP, please try again after an hour',
+		max: 10, // max requests
+		message: 'Too many accounts created from this IP, please try again after an hour',
 	});
 
 export const UpdateUserRateLimiter = () =>
 	rateLimit({
 		windowMs: 60 * 60 * 1000, // 1 hour window
-		max: 10,
+		max: 15,
 		message: 'Too many updates made from this IP, please try again later.',
 	});
