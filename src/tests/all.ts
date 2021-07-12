651570fa0165d7bb472b0a0844c87022a3aaf972
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-12 16:08:55 -0300

    chore: removes all tests from a single file

---
+++ src/tests/all.ts
@@ -0,0 +1,38 @@
+// import {
+// 	CreateUserTest,
+// 	ReadUserTest,
+// 	UpdateUserTest,
+// 	DeleteUserTest,
+// 	AddressTest,
+// 	CartTest,
+// 	forgetPasswordTest,
+// } from './unit/user';
+// import { emailTest, usernameTest, tokenTest } from './unit/auth';
+// import { IPTest } from './unit/IpBan';
+// import { dashSession, loadFileTest } from './unit/dashboard';
+// import { CreateItemTest } from './unit/item';
+
+// import { prisma } from '@src/prisma';
+// afterEach(async () => {
+// 	await prisma.address.deleteMany();
+// 	await prisma.cart.deleteMany();
+// });
+
+// describe('token creation', tokenTest);
+// describe('username auth', usernameTest);
+// describe('email auth', emailTest);
+
+// describe('Create user', CreateUserTest);
+// describe('Read user', ReadUserTest);
+// describe('Update user', UpdateUserTest);
+// describe('Delete user', DeleteUserTest);
+
+// describe('forget password', forgetPasswordTest);
+// describe('Address', AddressTest);
+// describe('Cart', CartTest);
+
+// describe('IP ban', IPTest);
+// describe('Load Admin users', loadFileTest);
+// describe('Admin/Dash session', dashSession);
+
+// describe('Create item', CreateItemTest);
