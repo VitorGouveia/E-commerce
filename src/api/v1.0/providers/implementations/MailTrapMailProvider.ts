16c1b25447f86b19fd851261169492c205a688b7
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-14 19:14:36 -0300

    fix: add back email provider

--- src/api/v1.0/providers/implementations/MailTrapMailProvider.ts
+++ src/api/v1.0/providers/implementations/MailTrapMailProvider.ts
@@ -27,18 +27,17 @@ export class MailTrapMailProvider implements IMailProvider {
 	}
 
 	async sendMail(message: IMessage): Promise<void> {
-		// await this.transporter.sendMail({
-		// 	to: {
-		// 		name: message.to.name,
-		// 		address: message.to.email,
-		// 	},
-		// 	from: {
-		// 		name: message.from.name,
-		// 		address: message.from.email,
-		// 	},
-		// 	subject: message.subject,
-		// 	html: message.body,
-		// });
-		// console.log(message);
+		await this.transporter.sendMail({
+			to: {
+				name: message.to.name,
+				address: message.to.email,
+			},
+			from: {
+				name: message.from.name,
+				address: message.from.email,
+			},
+			subject: message.subject,
+			html: message.body,
+		});
 	}
 }
