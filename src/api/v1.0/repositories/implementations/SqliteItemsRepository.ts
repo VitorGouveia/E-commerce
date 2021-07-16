f20a3fcca3464e4ad656c380c95782f83f30d100
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-16 01:22:20 -0300

    chore: moves average calculation to item repository

--- src/api/v1.0/repositories/implementations/SqliteItemsRepository.ts
+++ src/api/v1.0/repositories/implementations/SqliteItemsRepository.ts
@@ -1,243 +1,261 @@
-import { IItemsRepository } from "@v1/repositories"
-import { Item, Rating, Image } from "@v1/entities"
+import { IItemsRepository } from '@v1/repositories';
+import { Item, Rating, Image } from '@v1/entities';
 
-import { Item as ItemType, Rating as RatingType } from "@prisma/client"
-import { prisma } from "@src/prisma"
+import { Item as ItemType, Rating as RatingType } from '@prisma/client';
+import { prisma } from '@src/prisma';
 
 export class SqliteItemsRepository implements IItemsRepository {
-  async findById(id: number): Promise<Omit<ItemType, "image" | "rating"> | null> {
-    const item = await prisma.item.findUnique({
-      where: {
-        id
-      },
+	async findById(id: number): Promise<Omit<ItemType, 'image' | 'rating'> | null> {
+		const item = await prisma.item.findUnique({
+			where: {
+				id,
+			},
 
-      include: {
-        rating: {
-          select: {
-            id: true,
-            one_star: true,
-            two_star: true,
-            three_star: true,
-            four_star: true,
-            five_star: true,
-          }
-        },
-        image: {
-          select: {
-            id: true,
-            link: true
-          }
-        }
-      }
-    })
-    
-    return item
-  }
+			include: {
+				rating: {
+					select: {
+						id: true,
+						average: true,
+						one_star: true,
+						two_star: true,
+						three_star: true,
+						four_star: true,
+						five_star: true,
+					},
+				},
+				image: {
+					select: {
+						id: true,
+						link: true,
+					},
+				},
+			},
+		});
 
-  async findAll(category?: string, property?: string, sort?: "asc" | "desc" | string): Promise<ItemType[]> {
-    const items = await prisma.item.findMany()
-    
-    if(category != "undefined" && property != undefined && sort != undefined) {
-      const items = await prisma.item.findMany({
-        orderBy: [{
-          [property]: sort
-        }],
-        where: {
-          category
-        }
-      })
-      
-      return items
-    }
+		return item;
+	}
 
-    if(category != "undefined") {
-      const items = await prisma.item.findMany({
-        where: {
-          category
-        }
-      })
+	async findAll(
+		category?: string,
+		property?: string,
+		sort?: 'asc' | 'desc' | string
+	): Promise<ItemType[]> {
+		const items = await prisma.item.findMany();
 
-      return items
-    }
+		if (category != 'undefined' && property != undefined && sort != undefined) {
+			const items = await prisma.item.findMany({
+				orderBy: [
+					{
+						[property]: sort,
+					},
+				],
+				where: {
+					category,
+				},
+			});
 
-    if(property != undefined && sort != undefined) {
-      const items = await prisma.item.findMany({
-        orderBy: [{
-          [property]: sort
-        }]
-      })
-      
-      return items
-    }
+			return items;
+		}
 
+		if (category != 'undefined') {
+			const items = await prisma.item.findMany({
+				where: {
+					category,
+				},
+			});
 
-    return items
-  }
+			return items;
+		}
 
-  async findAllPagination(page: number, quantity: number, category?: string, property?: string, sort?: string): Promise<ItemType[] | Item[] | []> {
-    const items = await prisma.item.findMany({
-      take: quantity,
-      skip: quantity * page
-    })
+		if (property != undefined && sort != undefined) {
+			const items = await prisma.item.findMany({
+				orderBy: [
+					{
+						[property]: sort,
+					},
+				],
+			});
 
-    if(category != "undefined" && property != undefined && sort != undefined) {
-      const items = await prisma.item.findMany({
-        orderBy: [{
-          [property]: sort
-        }],
-        where: {
-          category
-        },
-        take: quantity,
-        skip: quantity * page
-      })
-      
-      return items
-    }
+			return items;
+		}
 
-    if(category != "undefined") {
-      const items = await prisma.item.findMany({
-        where: {
-          category
-        },
-        take: quantity,
-        skip: quantity * page
-      })
+		return items;
+	}
 
-      return items
-    }
+	async findAllPagination(
+		page: number,
+		quantity: number,
+		category?: string,
+		property?: string,
+		sort?: string
+	): Promise<ItemType[] | Item[] | []> {
+		const items = await prisma.item.findMany({
+			take: quantity,
+			skip: quantity * page,
+		});
 
-    if(property != undefined && sort != undefined) {
-      const items = await prisma.item.findMany({
-        orderBy: [{
-          [property]: sort
-        }],
-        take: quantity,
-        skip: quantity * page
-      })
-      
-      return items
-    }
+		if (category != 'undefined' && property != undefined && sort != undefined) {
+			const items = await prisma.item.findMany({
+				orderBy: [
+					{
+						[property]: sort,
+					},
+				],
+				where: {
+					category,
+				},
+				take: quantity,
+				skip: quantity * page,
+			});
 
-    return items
-  }
+			return items;
+		}
 
-  async rate(rating: Rating): Promise<Rating> {
-    const {
-      item_id,
-      one_star,
-      two_star,
-      three_star,
-      four_star,
-      five_star
-    } = rating
+		if (category != 'undefined') {
+			const items = await prisma.item.findMany({
+				where: {
+					category,
+				},
+				take: quantity,
+				skip: quantity * page,
+			});
 
-    const oldRating = await prisma.rating.findMany({
-      where: {
-        item_id
-      }
-    })
+			return items;
+		}
 
-    if(oldRating == undefined || oldRating == null) {
-      const itemRating = await prisma.rating.create({
-        data: {
-          item_id,
-          one_star,
-          two_star,
-          three_star,
-          four_star,
-          five_star
-        }
-      })
+		if (property != undefined && sort != undefined) {
+			const items = await prisma.item.findMany({
+				orderBy: [
+					{
+						[property]: sort,
+					},
+				],
+				take: quantity,
+				skip: quantity * page,
+			});
 
-      return itemRating
-    }
+			return items;
+		}
 
-    const itemRating = await prisma.rating.update({
-      where: {
-        id: 1
-      },
+		return items;
+	}
 
-      data: {
-        item_id,
-        one_star: one_star + oldRating[0].one_star,
-        two_star: two_star + oldRating[0].two_star,
-        three_star: three_star + oldRating[0].three_star,
-        four_star: four_star + oldRating[0].four_star,
-        five_star: five_star + oldRating[0].five_star
-      }
-    })
+	async rate(rating: Rating): Promise<Rating> {
+		const { item_id, one_star, two_star, three_star, four_star, five_star } = rating;
+		const oldRating = await prisma.rating.findMany({
+			where: {
+				item_id,
+			},
+		});
 
-    return itemRating
-  }
-  
-  async update(id: number, { ...props }: Item): Promise<ItemType> {
-    const item = await prisma.item.update({
-      where: {
-        id
-      },
+		const allNumberRatings =
+			one_star * 1 + two_star * 2 + three_star * 3 + four_star * 4 + five_star * 5;
 
-      data: {
-        ...props
-      }
-    })
+		const allRatings = one_star + two_star + three_star + four_star + five_star;
+		const average = allNumberRatings / allRatings;
 
-    return item
-  }
+		if (oldRating == undefined || oldRating == null || oldRating.length == 0) {
+			const itemRating = await prisma.rating.create({
+				data: {
+					item_id,
+					average,
+					one_star,
+					two_star,
+					three_star,
+					four_star,
+					five_star,
+				},
+			});
 
-  async save(item: Item): Promise<void> {
-    if(item.image.length > 0) {
-      const { image, ...props } = item
+			return itemRating;
+		}
 
-      const newItem = await prisma.item.create({
-        data: {
-          ...props,
-        }
-      })
+		const itemRating = await prisma.rating.update({
+			where: {
+				id: oldRating[0].id,
+			},
 
-      image.forEach(async imageInfo => {
-        await prisma.item.update({
-          where: {
-            id: newItem.id
-          },
-            
-          data: {
-            image: {
-              create: {
-                link: imageInfo.link
-              }
-            }
-          }
-        })
-      })
-    }
+			data: {
+				item_id,
+				average,
+				one_star: one_star + oldRating[0].one_star,
+				two_star: two_star + oldRating[0].two_star,
+				three_star: three_star + oldRating[0].three_star,
+				four_star: four_star + oldRating[0].four_star,
+				five_star: five_star + oldRating[0].five_star,
+			},
+		});
 
-    const { image, ...props } = item
+		return itemRating;
+	}
 
-    await prisma.item.create({
-      data: {
-        ...props
-      }
-    })
-  }
+	async update(id: number, { ...props }: Item): Promise<ItemType> {
+		const item = await prisma.item.update({
+			where: {
+				id,
+			},
 
-  async delete(id: number): Promise<void> {
-    await prisma.rating.deleteMany({
-      where: {
-        item_id: id
-      }
-    })
+			data: {
+				...props,
+			},
+		});
 
-    await prisma.image.deleteMany({
-      where: {
-        item_id: id
-      }
-    })
+		return item;
+	}
 
-    await prisma.item.delete({
-      where: {
-        id
-      }
-    })
-  }
+	async save(item: Item): Promise<void> {
+		if (item.image.length > 0) {
+			const { image, ...props } = item;
+
+			const newItem = await prisma.item.create({
+				data: {
+					...props,
+				},
+			});
+
+			image.forEach(async imageInfo => {
+				await prisma.item.update({
+					where: {
+						id: newItem.id,
+					},
+
+					data: {
+						image: {
+							create: {
+								link: imageInfo.link,
+							},
+						},
+					},
+				});
+			});
+		}
+
+		const { image, ...props } = item;
+
+		await prisma.item.create({
+			data: {
+				...props,
+			},
+		});
+	}
+
+	async delete(id: number): Promise<void> {
+		await prisma.rating.deleteMany({
+			where: {
+				item_id: id,
+			},
+		});
+
+		await prisma.image.deleteMany({
+			where: {
+				item_id: id,
+			},
+		});
+
+		await prisma.item.delete({
+			where: {
+				id,
+			},
+		});
+	}
 }
--- src/api/v1.0/services/item/RateItem.ts
+++ src/api/v1.0/services/item/RateItem.ts
@@ -1,70 +1,50 @@
-import { Request } from "express"
+import { Request } from 'express';
 
-import { IItemsRepository } from "@v1/repositories"
-import { SqliteItemsRepository } from "@v1/repositories/implementations"
+import { IItemsRepository } from '@v1/repositories';
+import { SqliteItemsRepository } from '@v1/repositories/implementations';
 
-import { Rating } from "@v1/entities"
+import { Rating } from '@v1/entities';
 
 class RateItemService {
-  constructor(
-    private itemsRepository: IItemsRepository
-  ) {}
+	constructor(private itemsRepository: IItemsRepository) {}
 
-  async rate(id: number, rateItemRequest: Rating) {
-    try {
-      const rating = new Rating(rateItemRequest, id)
+	async rate(id: number, rateItemRequest: Rating) {
+		try {
+			const rating = new Rating(rateItemRequest, id);
+			const { average, one_star, two_star, three_star, four_star, five_star } =
+				await this.itemsRepository.rate(rating);
 
-      const {
-        one_star,
-        two_star,
-        three_star,
-        four_star,
-        five_star
-      } = await this.itemsRepository.rate(rating)
+			const item = { one_star, two_star, three_star, four_star, five_star };
 
-      const item = { one_star, two_star, three_star, four_star, five_star }
-
-      const allNumberRatings = (
-        (one_star * 1) +
-        (two_star * 2) +
-        (three_star * 3) +
-        (four_star * 4) +
-        (five_star * 5)
-      )
-
-      const allRatings = one_star + two_star + three_star + four_star + five_star
-
-      const average = Math.floor(allNumberRatings / allRatings)
-
-      return {
-        item,
-        average
-      }
-    } catch (error) {
-      throw new Error(error.message)
-    }
-  }
+			return {
+				item,
+				average,
+			};
+		} catch (error) {
+			console.log(error);
+			throw new Error(error.message);
+		}
+	}
 }
 
 export default async (request: Request) => {
-  try {
-    const ItemsRepository = new SqliteItemsRepository()
-    const RateItem = new RateItemService(ItemsRepository)
+	try {
+		const ItemsRepository = new SqliteItemsRepository();
+		const RateItem = new RateItemService(ItemsRepository);
 
-    const { item, average } = await RateItem.rate(Number(request.params.id), request.body)
+		const { item, average } = await RateItem.rate(Number(request.params.id), request.body);
 
-    return ({
-      status: 200,
-      item,
-      average,
-      message: "Rated item with success!"
-    })
-    
-  } catch (error) {
-    return ({
-      error: true,
-      status: 400,
-      message: "Failed to rate item."
-    })
-  }
-}
+		return {
+			status: 200,
+			item,
+			average,
+			message: 'Rated item with success!',
+		};
+	} catch (error) {
+		return {
+			error: true,
+			status: 400,
+			message: 'Failed to rate item.',
+		};
+	}
+};
