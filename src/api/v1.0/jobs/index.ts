13a0d48fcbd2ce7f5582db9ad67b2a5ce7f32f96
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-18 21:03:29 -0300

    :package: add: Activation and types for jobs

--- src/api/v1.0/jobs/index.ts
+++ src/api/v1.0/jobs/index.ts
@@ -1,1 +1,22 @@
-export { default as RegistrationMail } from './RegistrationMail';
+import Queue from 'bull';
+
+export { RegistrationMail } from './RegistrationMail';
+export { ActivationMail } from './ActivationMail';
+
+export type DataType = {
+	user: {
+		name: string;
+		email: string;
+	};
+	token?: string;
+};
+
+export type JobsTypes = 'RegistrationMail' | 'ActivationMail';
+export type JobsData = DataType;
+export type JobsOptions = Queue.JobOptions;
+
+export type Job<T> = {
+	key: 'RegistrationMail' | 'ActivationMail';
+	options: JobsOptions;
+	handle: Queue.ProcessCallbackFunction<T>;
+};
