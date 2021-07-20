68f9b519e36a8fad023284b6385923ac3f5e7f31
vitor <vitor.gouveia@aluno.etecjrm.com.br>
2021-07-19 22:57:43 -0300

    chore: add list method to cart repository

--- src/api/v1.0/repositories/ICartRepository.ts
+++ src/api/v1.0/repositories/ICartRepository.ts
@@ -1,4 +1,7 @@
+import { Cart } from '@prisma/client';
+
 export interface ICartRepository {
-  save(user_id: string, item_id: number): Promise<void>
-  delete(user_id: string, id?: number): Promise<void>
+	list(user_id: string): Promise<Cart[]>;
+	save(user_id: string, item_id: number): Promise<void>;
+	delete(user_id: string, id?: number): Promise<void>;
 }
--- src/api/v1.0/repositories/implementations/SqliteCartRepository.ts
+++ src/api/v1.0/repositories/implementations/SqliteCartRepository.ts
@@ -1,33 +1,44 @@
-import { ICartRepository } from "@v1/repositories"
+import { ICartRepository } from '@v1/repositories';
 
-import { prisma } from "@src/prisma"
+import { Cart } from '@prisma/client';
+import { prisma } from '@src/prisma';
 
 export class SqliteCartRepository implements ICartRepository {
-  async save(user_id: string, item_id: number): Promise<void> {
-    await prisma.cart.create({
-      data: {
-        item_id,
-        user_id
-      }
-    })
-  }
+	async list(user_id: string): Promise<Cart[]> {
+		const cart = await prisma.cart.findMany({
+			where: {
+				user_id,
+			},
+		});
 
-  async delete(user_id: string, id?: number): Promise<void> {
-    if(id != undefined && user_id != undefined) {
-      await prisma.cart.deleteMany({
-        where: {
-          id,
-          user_id
-        }
-      })
+		return cart;
+	}
 
-      return
-    }
+	async save(user_id: string, item_id: number): Promise<void> {
+		await prisma.cart.create({
+			data: {
+				item_id,
+				user_id,
+			},
+		});
+	}
 
-    await prisma.cart.deleteMany({
-      where: {
-        user_id
-      }
-    })
-  }
+	async delete(user_id: string, id?: number): Promise<void> {
+		if (id !== undefined) {
+			await prisma.cart.deleteMany({
+				where: {
+					id,
+					user_id,
+				},
+			});
+
+			return;
+		}
+
+		await prisma.cart.deleteMany({
+			where: {
+				user_id,
+			},
+		});
+	}
 }
