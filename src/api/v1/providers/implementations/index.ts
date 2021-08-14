a6c30a894abe8dc0bccd001cba5f52fe7c1a8a03
vitorgouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-08-14 17:38:38 -0300

    fix(providers): add newly added providers to imports barrel

--- src/api/v1/providers/implementations/index.ts
+++ src/api/v1/providers/implementations/index.ts
@@ -1,3 +1,4 @@
-import { MailTrapMailProvider } from './MailTrapMailProvider';
-
-export { MailTrapMailProvider };
+export { MailTrapMailProvider } from './MailTrapMailProvider';
+export { ValidatorValidatorProvider } from './ValidatorValidatorProvider';
+export { BcryptHashPasswordProvider } from './BcryptHashPasswordProvider';
+export { JWTTokenProvider } from './JWTTokenProvider';
--- src/api/v1/providers/index.ts
+++ src/api/v1/providers/index.ts
@@ -1,1 +1,4 @@
 export { IMailProvider } from './IMailProvider';
+export { IValidator } from './IValidator';
+export { IHashPassword } from './IHashPassword';
+export { IToken } from './IToken';
